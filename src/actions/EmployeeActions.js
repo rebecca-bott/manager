c95ce64363719c95be0068cd673d889fc78c31b3
Rebecca Bott <rebecca.bott@agencylabs.com>
2019-07-31 16:07:56 -0400

    More changes

--- src/actions/EmployeeActions.js
+++ src/actions/EmployeeActions.js
@@ -1,6 +1,10 @@
+import firebase from 'firebase';
 import {
-    EMPLOYEE_UPDATE
+    EMPLOYEE_UPDATE,
+    EMPLOYEE_CREATE,
+    EMPLOYEE_FETCH_SUCCESS
 } from './types';
+import { Actions } from 'react-native-router-flux';
 
 export const employeeUpdate = ({ prop, value }) => {
     return {
@@ -8,3 +12,27 @@ export const employeeUpdate = ({ prop, value }) => {
         payload: { prop, value }
     };
 };
+
+export const employeeCreate = ({ name, phone, shift }) => {
+    const { currentUser } = firebase.auth();
+
+    return (dispatch) => {
+  firebase.database().ref(`/users/${currentUser.uid}/employee`)
+  .push({ name, phone, shift })
+  .then(() => {
+      dispatch({ type: EMPLOYEE_CREATE });
+      Actions.employeeList({ type: 'reset' })  
+});
+};
+};
+
+export const employeeFetch = () => {
+    const { currentUser } = firebase.auth();
+
+    return (dispatch) => {
+        firebase.database().ref(`/users/${currentUser.uid}/employee`)
+        .on('value', snapshot => {
+            dispatch({ type: EMPLOYEE_FETCH_SUCCESS, payload: snapshot.val() })
+        })
+    }
+}
--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -1,17 +1,38 @@
-import React, {Component} from 'react';
-import { View, Text } from 'react-native';
+import _ from 'lodash';
+import {View, FlatList} from 'react-native';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { employeeFetch } from '../actions';
+import ListItem from './ListItem';
 
 class EmployeeList extends Component {
-    render() {
-        return (
-            <View>
-                <Text>Employee List</Text>
-                <Text>Employee List</Text>
-                <Text>Employee List</Text>
-                <Text>Employee List</Text>
-            </View>
-        );
-    }
+  componentWillMount() {
+    this.props.employeeFetch();
+
+  }
+
+  renderItem({ item }) {
+     return <ListItem employee={item} />
+  }
+
+  render() {
+    return (
+      <View>
+      <FlatList
+        data={this.props.employee}
+        keyExtractor={(item) => item.key} renderItem={this.renderItem}
+      />
+      </View>
+    );
+  }
 }
 
-export default EmployeeList;
+const mapStateToProps = state => {
+ 
+  const employee = _.map(state.employee, (val, uid) => {
+    return { ...val, uid};
+  })
+ return {employee}
+
+}
+export default connect(mapStateToProps, { employeeFetch })(EmployeeList);
